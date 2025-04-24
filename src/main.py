b8a0f8e3b20b0cfc17ec0c4bfae58f0f80ecd3de
Dribze <ezbirdeleven@yahoo.com>
2025-04-24 18:17:55 -0500

    ammended several errors including nojekyll

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import sys
 from gencontent import generate_page
 from textnode import TextNode, TextType
 from gencontent import generate_pages_recursive
@@ -5,16 +6,19 @@ import os
 import shutil
 from copystatic import copy_files_recursive
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
 template_path = "./template.html"
-
+default_basepath = "/"
 def main():
+	basepath = default_basepath
+	if len(sys.argv) > 1:
+		basepath = sys.argv[1]
 	print("deleting public directory....")
 	print("copying files from static to public directory...")
 	copy_files_recursive(dir_path_static, dir_path_public)
 	print("generating page...")
-	generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+	generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 
